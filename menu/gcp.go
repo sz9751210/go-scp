721535759f982bc35d2ee3d72c0b1173149dbe24
Alan <sz9751210@gmail.com>
2023-09-03 19:31:50 +0800

    get, start, stop gce

---
+++ menu/gcp.go
@@ -0,0 +1,72 @@
+package menu
+
+import (
+	"fmt"
+	"go-ssh-util/actions/gcp"
+
+	"github.com/trzsz/promptui"
+)
+
+func showGCPMenu() int {
+	menuPrompt := promptui.Select{
+		Label: "Choose an operation:",
+		Items: []string{"GCE", "Return to Main Menu"},
+	}
+
+	GCPIndex, _, err := menuPrompt.Run()
+	if err != nil {
+		fmt.Println("Error:", err)
+		return -1
+	}
+
+	return GCPIndex
+}
+
+func RunGCP() {
+	GCPActive := true
+
+	for GCPActive {
+		GCPIndex := showGCPMenu()
+
+		switch GCPIndex {
+		case 0:
+			RunGCE()
+		default:
+			GCPActive = false
+		}
+	}
+}
+
+func showGCEMenu() int {
+	menuPrompt := promptui.Select{
+		Label: "Choose an operation:",
+		Items: []string{"Get VMs", "Start VM", "Stop VM", "Return to Main Menu"},
+	}
+
+	GCPIndex, _, err := menuPrompt.Run()
+	if err != nil {
+		fmt.Println("Error:", err)
+		return -1
+	}
+
+	return GCPIndex
+}
+
+func RunGCE() {
+	GCPActive := true
+
+	for GCPActive {
+		GCPIndex := showGCEMenu()
+
+		switch GCPIndex {
+		case 0:
+			gcp.RunGetVMs()
+		case 1:
+			gcp.RunStartVM()
+		case 2:
+			gcp.RunStopVM()
+		default:
+			GCPActive = false
+		}
+	}
+}
--- menu/main_menu.go
+++ menu/main_menu.go
@@ -10,7 +10,7 @@ import (
 func showMainMenu() int {
 	menuPrompt := promptui.Select{
 		Label: "Choose an operation:",
-		Items: []string{"\U0001F4C1 Copy File/Directory (SCP)", "\U0001F5A5  Check Systeminfo", "\U0001F4E1 Check Network", "\U0001F433 Check Container", "\U0001F511 SSH", "Quit"},
+		Items: []string{"\U0001F4C1 Copy File/Directory (SCP)", "\U0001F5A5  Check Systeminfo", "\U0001F4E1 Check Network", "\U0001F433 Check Container", "\U0001F511 SSH", "\U0001F30E Cloud", "Quit"},
 	}
 
 	scpIndex, _, err := menuPrompt.Run()
@@ -38,6 +38,8 @@ func RunMainLoop() {
 		case 4:
 			actions.RunSSH()
 		case 5:
+			RunCloud()
+		case 6:
 			fmt.Println("Goodbye!")
 			return
 		}
