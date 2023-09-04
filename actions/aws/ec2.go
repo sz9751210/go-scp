39389632b66277fb2fb286def76b58ea01b8f7ff
Elmer_Lee <elmer_lee@mail.chungyo.net>
2023-09-04 16:43:13 +0800

    新增AWS GET 機器資訊

---
+++ actions/aws/ec2.go
@@ -0,0 +1,22 @@
+package aws
+
+import (
+	"fmt"
+	"go-ssh-util/config"
+	"go-ssh-util/ssh"
+	"os"
+)
+
+func RunGetVMs() {
+	selectedHost, ExecutionMode, err := config.ChooseAlias()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+		return
+	}
+	command := `aws ec2 describe-instances --region ap-southeast-2 --query "Reservations[*].Instances[*].Tags[?Key=='Name'].Value" --output text | cat`
+	if ExecutionMode == 1 {
+		ssh.ExecuteLocalCommand(command)
+	} else {
+		ssh.ExecuteRemoteCommand(command, fmt.Sprintf("%s@%s", selectedHost.User, selectedHost.Host), selectedHost.Port)
+	}
+}
